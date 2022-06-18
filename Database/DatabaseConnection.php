739aadccd3f698758af7bbe5426e88fb004f63b1
ghislainvava <ghislainvachet@yahoo.fr>
2022-06-18 14:36:55 +0200

    dotenv

--- Database/DatabaseConnection.php
+++ Database/DatabaseConnection.php
@@ -2,6 +2,7 @@
 namespace BlogOC\Database;
 
 use PDO;
+use Dotenv;
 
 class DatabaseConnection
 {
@@ -9,10 +10,20 @@ class DatabaseConnection
     
     public function getConnection(): PDO
     {
+        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/..');
+        $dotenv->load();
+        $host = $_ENV['DATABASE_HOST'];
+        $user = $_ENV['DATABASE_USER'];
+        $password = $_ENV['DATABASE_PASSWORD'];
+        $database = $_ENV['DATABASE'];
         if ($this->pdo === null) {
-            $this->pdo = new PDO('mysql:host=127.0.0.1;dbname=BlogOC;charset=utf8', 'root', '',
-            [ PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
+            $this->pdo = new PDO(
+                'mysql:host=' .$host.';dbname='.$database.';charset=utf8',
+                $user,
+                $password,
+                [ PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]
+            );
         }
-        return $this->pdo; 
+        return $this->pdo;
     }
 }
