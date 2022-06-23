537d5e2fbcd16eafb71be664fe2ac892a6a82d51
ghislainvava <ghislainvachet@yahoo.fr>
2022-06-23 10:29:48 +0200

    codacy15

--- Views/show-article.php
+++ Views/show-article.php
@@ -1,11 +1,11 @@
 <div class="container">
     <div class="container">
-        <div class="mt-5" style="background-image:url(<?=htmlspecialchars_decode($article->image)?>)"></div>
-        <h1 class="mb-5"><?=utf8_decode($article->title)?></h1>
+        <div class="mt-5" style="background-image:url(<?=htmlspecialchars($article->image)?>)"></div>
+        <h1 class="mb-5"><?=htmlspecialchars_decode($article->title)?></h1>
         <?php if (htmlspecialchars_decode($article->image !== '')) :?>
             <img  style="width: 350px;" src="images/<?=htmlspecialchars_decode($article->image)?>" />
         <?php endif; ?>
-        <h2 class="mt-5 mb-3"><?=utf8_decode($article->chapo)?></h2>
+        <h2 class="mt-5 mb-3"><?=htmlspecialchars_decode($article->chapo)?></h2>
         <p ><?= utf8_decode($article->content) ?></p>
         <p class="fst-italic mb-5">Post émis part : <?=htmlspecialchars_decode($article->firstname). ' ' .htmlspecialchars_decode($article->lastname)?>
             <br>Mis à jour le : <?= htmlspecialchars_decode($article->date)?>
@@ -13,7 +13,7 @@
         <?php if ($currentUser['admin'] == true) : ?>
            <div class="action">
                 <a class="btn btn-secondary" href="index.php?page=delete-article&id=<?=htmlspecialchars_decode($article->id)?>">Supprimer</a> 
-                <a class="btn btn-primary" href="index.php?page=form-article&id=<?=htmlspecialchars_decode($article->id)?>">Editer l'article</a>
+                <a class="btn btn-primary" href="index.php?page=form-article&id=<?=htmlspecialchars($article->id)?>">Editer l'article</a>
             </div> 
             <?php endif;?>
     </div>
@@ -24,7 +24,7 @@
             <?php if ($comment->checked > 0) : ?> 
             <div class=" mb-5">
                     <p><?=$comment->commentaire?></p>
-                    <p class="fst-italic">Ecrit le : <?=htmlspecialchars_decode($comment->date_commentaire)?>&nbsp &nbsp par : <?=htmlspecialchars($comment->firstname).'   '.htmlspecialchars($comment->lastname)?></p>   
+                    <p class="fst-italic">Ecrit le : <?=htmlspecialchars($comment->date_commentaire)?>&nbsp &nbsp par : <?=htmlspecialchars($comment->firstname).'   '.htmlspecialchars($comment->lastname)?></p>   
                     <p class="text-center">*****</p>
                     <?php if ($currentUser['admin'] == 1) : ?>
                     <div class="action">
